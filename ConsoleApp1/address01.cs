4ffe0dd5eb77fce1d61096f06b7834ec558c29d9
JANGSAEHYEON <rokuel6l@metaskyworld.net>
2025-04-28 22:49:03 +0900

    dns address

---
+++ ConsoleApp1/address01.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Net;
+
+namespace IPAdress01
+{
+    class address01
+    {
+        // static void Main(string[] args)
+        // {
+        //     string adress = Console.ReadLine();
+        //     IPAddress IP = IPAddress.Parse(adress);
+        //     Console.WriteLine($"ip : {IP.ToString()}");
+        // }
+    }
+}
---
+++ ConsoleApp1/dns01.cs
@@ -0,0 +1,16 @@
+﻿using System.Net;
+
+namespace Dns01
+{
+    public class dns01
+    {
+        static void Main(string[] args)
+        {
+            IPAddress[] IP = Dns.GetHostAddresses("www.naver.com");
+            foreach (var HostIP in IP)
+            {
+                Console.WriteLine($"{HostIP}");
+            }
+        }
+    }
+}
